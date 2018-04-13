8722f9192a59505738dbc7aa26a60f38d0712206
HansenK <thiagohansenklein@gmail.com>
2018-04-13 09:58:57 -0300

    Edit How to show clicked operation

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
 		this.state = {
 			valueIn: 0,
 			valA: 0,
-			opSelect: null,
+			opSelected: null,
 			list: []
 		}
 	}
@@ -17,7 +17,7 @@ class App extends Component {
 	
 		let valueIn = this.state.valueIn;
 		let valA = this.state.valA; 
-		let opSelect = this.state.opSelect;
+		let opSelected = this.state.opSelected;
 		let list = this.state.list;
 		let resHist;
 		const btOp = ['+','-','*','/','='];
@@ -25,13 +25,10 @@ class App extends Component {
 		if(valueB === 'C'){
 			valueIn = 0;
 			valA = 0;
-			opSelect = null;
+			opSelected = null;
 			list.push('*Clear*');
-			btOp.map((opr => {
-				document.getElementById(opr).style.backgroundColor = 'rgb(233, 133, 19)';
-				document.getElementById(opr).style.border = 'none';
-			}));
 		}
+
 		if(typeof valueB === 'number' || valueB === '.'){
 			if(valueIn === 0){
 				valueIn = valueB;
@@ -46,78 +43,65 @@ class App extends Component {
 			if(valueIn !== 0 && valA === 0){
 				valA = valueIn;
 				valueIn = 0;
-				opSelect = valueB;
+				opSelected = valueB;
 			}
-
-			btOp.map((opr => {
-				if(opr === valueB){
-					document.getElementById(valueB).style.backgroundColor = "rgb(151, 87, 13)";
-					document.getElementById(valueB).style.border = '1.5px solid black';
-				}
-				else{
-					document.getElementById(opr).style.backgroundColor = "rgb(233, 133, 19)";
-					document.getElementById(opr).style.border = 'none';
-				}
-
-				if(opr === '='){
-					document.getElementById(opr).style.backgroundColor = "rgb(233, 133, 19)";
-					document.getElementById(opr).style.border = 'none';
-				}
-			}));
 
 			if(valueIn !== 0 && valA !== 0){
 				
-				if(this.state.opSelect === '+'){
-					resHist = parseFloat(valA, 10) +(this.state.opSelect)+ parseFloat(valueIn, 10);
+				if(this.state.opSelected === '+'){
+					resHist = parseFloat(valA, 10) +(this.state.opSelected)+ parseFloat(valueIn, 10);
 					valA = parseFloat(valA, 10) + parseFloat(valueIn, 10);
 					resHist = resHist +' = '+ valA;
 					list.push(resHist);
-					opSelect = valueB;
+					opSelected = valueB;
 				}
-				if(this.state.opSelect === '-'){
-					resHist = parseFloat(valA, 10) +(this.state.opSelect)+ parseFloat(valueIn, 10);
+				if(this.state.opSelected === '-'){
+					resHist = parseFloat(valA, 10) +(this.state.opSelected)+ parseFloat(valueIn, 10);
 					valA = parseFloat(valA, 10) - parseFloat(valueIn, 10);
 					resHist = resHist +' = '+ valA;
 					list.push(resHist);
-					opSelect = valueB;
+					opSelected = valueB;
 				}
-				if(this.state.opSelect === '*'){
-					resHist = parseFloat(valA, 10) +(this.state.opSelect)+ parseFloat(valueIn, 10);
+				if(this.state.opSelected === '*'){
+					resHist = parseFloat(valA, 10) +(this.state.opSelected)+ parseFloat(valueIn, 10);
 					valA = parseFloat(valA, 10) * parseFloat(valueIn, 10);
 					resHist = resHist +' = '+ valA;
 					list.push(resHist);
-					opSelect = valueB;
+					opSelected = valueB;
 				}
-				if(this.state.opSelect === '/'){
-					resHist = parseFloat(valA, 10) +(this.state.opSelect)+ parseFloat(valueIn, 10);
+				if(this.state.opSelected === '/'){
+					resHist = parseFloat(valA, 10) +(this.state.opSelected)+ parseFloat(valueIn, 10);
 					valA = parseFloat(valA, 10) / parseFloat(valueIn, 10);
 					resHist = resHist +' = '+ valA;
 					list.push(resHist);
-					opSelect = valueB;
+					opSelected = valueB;
 				}
-				if(opSelect === '='){
-					valueIn = parseFloat(parseFloat(valA, 10) +(this.state.opSelect)+ parseFloat(valueIn, 10));
+				if(opSelected === '='){
+					valueIn = parseFloat(parseFloat(valA, 10) +(this.state.opSelected)+ parseFloat(valueIn, 10));
 					valA=0;
 				}
 				
-				if(opSelect !== '='){
+				if(opSelected !== '='){
 					valueIn = 0;
 				}
 			}
 		}
-		this.setState({valueIn, valA, opSelect, list});
+		this.setState({valueIn, valA, opSelected, list});
 	}
 
 	render() {
 		const btNum = [1,2,3,4,5,6,7,8,9,'.',0,'C'];
 		const btOp = ['+','-','*','/','='];
+		let classNm;
 
 		const createButtonL = btNum.map((number, i) =>
 			<button key={i} value={number} className="num" id={number} onClick={ () => this.show(number) } >{ number }</button>
 		);
-		const createButtonR = btOp.map((opr, i) =>
-			<button key={i} value={opr} className="op" id={opr} onClick={ () => this.show(opr) } >{ opr }</button>
-		);
+		const createButtonR = btOp.map((opr, i) => {
+			opr === this.state.opSelected ? classNm = "clicked" : classNm = "op";
+			if(opr === '=') classNm = "op";
+			return <button key={i} value={opr} className={classNm} id={opr} onClick={ () => this.show(opr) } >{ opr }</button>
+		});
 		const createList = this.state.list.map((prevMath, i) => 
 			<li key={i} >{prevMath}</li>
 		);
